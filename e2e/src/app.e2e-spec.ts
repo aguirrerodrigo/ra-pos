7c4f62fc19096e19084c34a1e5acf3cafdb73517
aguirrerodrigo <raguirre.dev@gmail.com>
2020-05-11 23:49:15 +0800

    Renamed project to ra-pos

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('MergerKing-POS app is running!');
+    expect(page.getTitleText()).toEqual('ra-pos app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
       expect(app).toBeTruthy();
    });
 
-   it(`should have as title 'MergerKing-POS'`, () => {
+   it(`should have as title 'POS'`, () => {
       const fixture = TestBed.createComponent(AppComponent);
       const app = fixture.componentInstance;
-      expect(app.title).toEqual('MergerKing-POS');
+      expect(app.title).toEqual('POS');
    });
 
    it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
       fixture.detectChanges();
       const compiled = fixture.nativeElement;
       expect(compiled.querySelector('.content span').textContent).toContain(
-         'MergerKing-POS app is running!'
+         'ra-pos app is running!'
       );
    });
 });

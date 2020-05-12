5b156167c0f3c11f7444eedf131026c85d602491
aguirrerodrigo <raguirre.dev@gmail.com>
2020-05-12 23:30:54 +0800

    Removed no longer needed app.component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,13 +21,4 @@ describe('AppComponent', () => {
       const app = fixture.componentInstance;
       expect(app.title).toEqual('POS');
    });
-
-   it('should render title', () => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.nativeElement;
-      expect(compiled.querySelector('.content span').textContent).toContain(
-         'ra-pos app is running!'
-      );
-   });
 });
